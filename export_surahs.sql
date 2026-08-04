87121e0fbaa56648c7176e1adcd3fe68ae10f253
sham <shamalali7274@gmail.com>
2026-08-04 16:10:52 +0300

    Integrate OCR model with backend and data base

---
+++ export_surahs.sql
@@ -0,0 +1,4 @@
+-- Export of surahs
+TRUNCATE TABLE `surahs`;
+INSERT INTO `surahs` (`surah_id`,`surah_name`,`order_in_book`,`first_page`) VALUES (1,'سُورَةُ الْفَاتِحَةِ',1,1);
+INSERT INTO `surahs` (`surah_id`,`surah_name`,`order_in_book`,`first_page`) VALUES (2,'سُورَةُ البَقَرَةِ',2,2);
